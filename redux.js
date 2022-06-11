e46df338c031460be7e029ddd3593fbb4ae75f8e
lawal rashidi <lawalrashidi95@gmail.com>
2022-06-11 12:31:29 +0100

    latest commit

--- redux.js
+++ redux.js
@@ -628,7 +628,7 @@ render();
 
 
 // 9TH vid
-// array mutation with concat, slice nand spread
+// array immutation with concat, slice nand spread
 const adCounter = (list) => {
   //  list.push(0);
   return list.concat([0])
@@ -695,3 +695,431 @@ incrementCounter()
 
 
 // 10th vid
+// avoiding object mutation with object.assign()
+
+const toggleTodo = (todo) => {
+  // todo.completed = !todo.completed
+  // return todo;
+
+  return Object.assign({}, todo, {completed: !todo.completed})
+
+  or
+
+  return {...todo, completed: !todo.completed}
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  }; 
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore)
+
+  expect(
+    toggleTodo(todoBefore).toEqual(todoAfter)
+    )
+}
+
+testToggleTodo()
+console.log('All tests passed.')
+
+
+
+// 11th vid
+const todos = (state = [], action) => {
+   switch (action.type) {
+     case 'ADD_TODO':
+       return [
+         ...state, {
+           id: action.id,
+           text: action.text,
+           completed: false
+         }
+       ];
+       default:
+         return state;
+   }
+}
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+   deepFreeze(stateBefore);
+   deepFreeze(action);
+
+   expect(
+     todos(stateBefore, action).toEqual(stateAfter)
+   )
+}
+
+testAddTodo();
+console.log('All tests passed')
+
+
+
+
+// 12th vid
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        },
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo
+        } 
+        return {
+          ...todo, completed: !todo.completed
+        }
+      })
+    default:
+      return state;
+  }
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: "ADD_TODO",
+    id: 0,
+    text: "Learn Redux",
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action).toEqual(stateAfter));
+};
+
+
+const testToggleTodo = () => {
+
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false
+    }
+  ];
+
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false
+    },
+    {
+      id: 1,
+      text: "Go Shopping",
+      completed: true
+    },
+  ];
+
+   deepFreeze(stateBefore);
+   deepFreeze(action);
+
+   expect(todos(stateBefore, action).toEqual(stateAfter));
+}
+
+testAddTodo();
+testToggleTodo();
+console.log("All tests passed");
+
+
+
+
+// 13th vid
+// REDUCER COMPOSITION
+const todo = (state, action) => {
+   switch (action.type) {
+     case 'ADD_TODO':
+        return {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        };
+      case 'TOGGLE_TODO':
+        if (state.id !== action.id) {
+          return state;
+        }
+        return {
+          ...state,
+          completed: !state.completed,
+        };
+        default:
+          return state;
+   }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state, todo(undefined, action)
+        
+      ];
+    case "TOGGLE_TODO":
+      return state.map(t => 
+        todo(t, action)
+      );
+    default:
+      return state;
+  }
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: "ADD_TODO",
+    id: 0,
+    text: "Learn Redux",
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action).toEqual(stateAfter));
+};
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+    {
+      id: 1,
+      text: "Go Shopping",
+      completed: false,
+    },
+  ];
+
+  const action = {
+    type: "TOGGLE_TODO",
+    id: 1,
+  };
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+    {
+      id: 1,
+      text: "Go Shopping",
+      completed: true,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action).toEqual(stateAfter));
+};
+
+
+
+testAddTodo();
+testToggleTodo();
+console.log("All tests passed");
+
+
+
+
+// 14th vid
+// REDUCER COMPOSITION WITH OBJECTS
+const todo = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case "TOGGLE_TODO":
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed,
+      };
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, todo(undefined, action)];
+    case "TOGGLE_TODO":
+      return state.map((t) => todo(t, action));
+    default:
+      return state;
+  }
+};
+
+
+const visibilityFilter = (state = "SHOW_ALL", action) => {
+  switch (action.type) {
+    case "SET_VISIBILITY_FILTER":
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter, action
+    )
+  }
+}
+
+
+const { createStore } = Redux;
+const store = createStore(todoApp);
+store.getState()
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+})
+store.getState();
+store.dispatch({
+  type: "ADD_TODO",
+  id: 1,
+  text: "Go Shopping",
+});
+store.getState();
+store.dispatch({
+  type: "TOGGLE_TODO",
+  id: 0
+});
+store.dispatch({
+  type: "SET_VISIBILITY_FILTER",
+  filter: "SHOW_COMPLETED",
+});
+
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: "ADD_TODO",
+    id: 0,
+    text: "Learn Redux",
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action).toEqual(stateAfter));
+};
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+    {
+      id: 1,
+      text: "Go Shopping",
+      completed: false,
+    },
+  ];
+
+  const action = {
+    type: "TOGGLE_TODO",
+    id: 1,
+  };
+
+  const stateAfter = [
+    {
+      id: 0,
+      text: "Learn Redux",
+      completed: false,
+    },
+    {
+      id: 1,
+      text: "Go Shopping",
+      completed: true,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action).toEqual(stateAfter));
+};
+
+
+testAddTodo();
+testToggleTodo();
+console.log("All tests passed");
